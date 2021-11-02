52ebab454a586fae7af53f83f3c25481f1c59174
AiltonRafael <ailtonrafaeldiasjr@gmail.com>
2021-11-02 08:55:09 -0300

    pokedex updated

--- src/js/pokedex/index.js
+++ src/js/pokedex/index.js
@@ -25,196 +25,232 @@ export const pokedex = [{
   },
 },
 
-// {
-//   "id": 2,
-//   "name": {
-//     "english": "Ivysaur",
-//     "japanese": "フシギソウ",
-//     "chinese": "妙蛙草",
-//     "french": "Herbizarre"
-//   },
-//   "type": [
-//     "Grass",
-//     "Poison"
-//   ],
-//   "base": {
-//     "HP": 60,
-//     "Attack": 62,
-//     "Defense": 63,
-//     "Sp. Attack": 80,
-//     "Sp. Defense": 80,
-//     "Speed": 60
-//   },
-//   "special": ['absorve', 'bubble', 'agility', 'constrict']
-// },
-// {
-//   "id": 3,
-//   "name": {
-//     "english": "Venusaur",
-//     "japanese": "フシギバナ",
-//     "chinese": "妙蛙花",
-//     "french": "Florizarre"
-//   },
-//   "type": [
-//     "Grass",
-//     "Poison"
-//   ],
-//   "base": {
-//     "HP": 80,
-//     "Attack": 82,
-//     "Defense": 83,
-//     "Sp. Attack": 100,
-//     "Sp. Defense": 100,
-//     "Speed": 80
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp']
-// },
-// {
-//   "id": 4,
-//   "name": {
-//     "english": "Charmander",
-//     "japanese": "ヒトカゲ",
-//     "chinese": "小火龙",
-//     "french": "Salamèche"
-//   },
-//   "type": [
-//     "Fire"
-//   ],
-//   "base": {
-//     "HP": 39,
-//     "Attack": 52,
-//     "Defense": 43,
-//     "Sp. Attack": 60,
-//     "Sp. Defense": 50,
-//     "Speed": 65
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp'],
-// },
-// {
-//   "id": 5,
-//   "name": {
-//     "english": "Charmeleon",
-//     "japanese": "リザード",
-//     "chinese": "火恐龙",
-//     "french": "Reptincel"
-//   },
-//   "type": [
-//     "Fire"
-//   ],
-//   "base": {
-//     "HP": 58,
-//     "Attack": 64,
-//     "Defense": 58,
-//     "Sp. Attack": 80,
-//     "Sp. Defense": 65,
-//     "Speed": 80
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp']
-// },
-// {
-//   "id": 6,
-//   "name": {
-//     "english": "Charizard",
-//     "japanese": "リザードン",
-//     "chinese": "喷火龙",
-//     "french": "Dracaufeu"
-//   },
-//   "type": [
-//     "Fire",
-//     "Flying"
-//   ],
-//   "base": {
-//     "HP": 78,
-//     "Attack": 84,
-//     "Defense": 78,
-//     "Sp. Attack": 109,
-//     "Sp. Defense": 85,
-//     "Speed": 100
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp'],
-// },
-// {
-//   "id": 7,
-//   "name": {
-//     "english": "Squirtle",
-//     "japanese": "ゼニガメ",
-//     "chinese": "杰尼龟",
-//     "french": "Carapuce"
-//   },
-//   "type": [
-//     "Water"
-//   ],
-//   "base": {
-//     "HP": 44,
-//     "Attack": 48,
-//     "Defense": 65,
-//     "Sp. Attack": 50,
-//     "Sp. Defense": 64,
-//     "Speed": 43
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp']
-// },
-// {
-//   "id": 8,
-//   "name": {
-//     "english": "Wartortle",
-//     "japanese": "カメール",
-//     "chinese": "卡咪龟",
-//     "french": "Carabaffe"
-//   },
-//   "type": [
-//     "Water"
-//   ],
-//   "base": {
-//     "HP": 59,
-//     "Attack": 63,
-//     "Defense": 80,
-//     "Sp. Attack": 65,
-//     "Sp. Defense": 80,
-//     "Speed": 58
-//   },
-//   "special": ['bind', 'bubble', 'agility', 'clamp'],
-// },
-// {
-//   "id": 9,
-//   "name": {
-//     "english": "Blastoise",
-//     "japanese": "カメックス",
-//     "chinese": "水箭龟",
-//     "french": "Tortank"
-//   },
-//   "type": [
-//     "Water"
-//   ],
-//   "base": {
-//     "HP": 79,
-//     "Attack": 83,
-//     "Defense": 100,
-//     "Sp. Attack": 85,
-//     "Sp. Defense": 105,
-//     "Speed": 78
-//   }, "special": ['bind', 'bubble', 'agility', 'clamp'],
-// },
-// {
-//   "id": 10,
-//   "name": {
-//     "english": "Caterpie",
-//     "japanese": "キャタピー",
-//     "chinese": "绿毛虫",
-//     "french": "Chenipan"
-//   },
-//   "type": [
-//     "Bug"
-//   ],
-//   "base": {
-//     "HP": 45,
-//     "Attack": 30,
-//     "Defense": 35,
-//     "Sp. Attack": 20,
-//     "Sp. Defense": 20,
-//     "Speed": 45
-//   }, "special": ['bind', 'bubble', 'agility', 'clamp'],
-// }
+{
+  "id": 2,
+  "name": {
+    "english": "Ivysaur",
+    "japanese": "フシギソウ",
+    "chinese": "妙蛙草",
+    "french": "Herbizarre"
+  },
+  "type": [
+    "Grass",
+    "Poison"
+  ],
+  "base": {
+    "HP": 60,
+    "Attack": 62,
+    "Defense": 63,
+    "Sp. Attack": 80,
+    "Sp. Defense": 80,
+    "Speed": 60
+  },
+  "special": ['absorve', 'bubble', 'agility', 'constrict'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/ivysaur.png",
+    "front": "https://img.pokemondb.net/sprites/go/normal/ivysaur.png"
+  },
+},
+{
+  "id": 3,
+  "name": {
+    "english": "Venusaur",
+    "japanese": "フシギバナ",
+    "chinese": "妙蛙花",
+    "french": "Florizarre"
+  },
+  "type": [
+    "Grass",
+    "Poison"
+  ],
+  "base": {
+    "HP": 80,
+    "Attack": 82,
+    "Defense": 83,
+    "Sp. Attack": 100,
+    "Sp. Defense": 100,
+    "Speed": 80
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/diamond-pearl/back-normal/venusaur.png",
+    "front": "https://img.pokemondb.net/sprites/go/normal/venusaur-mega.png"
+  },
+},
+{
+  "id": 4,
+  "name": {
+    "english": "Charmander",
+    "japanese": "ヒトカゲ",
+    "chinese": "小火龙",
+    "french": "Salamèche"
+  },
+  "type": [
+    "Fire"
+  ],
+  "base": {
+    "HP": 39,
+    "Attack": 52,
+    "Defense": 43,
+    "Sp. Attack": 60,
+    "Sp. Defense": 50,
+    "Speed": 65
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/charmander.png",
+    "front": "https://img.pokemondb.net/sprites/home/normal/charmander.png"
+  },
+},
+{
+  "id": 5,
+  "name": {
+    "english": "Charmeleon",
+    "japanese": "リザード",
+    "chinese": "火恐龙",
+    "french": "Reptincel"
+  },
+  "type": [
+    "Fire"
+  ],
+  "base": {
+    "HP": 58,
+    "Attack": 64,
+    "Defense": 58,
+    "Sp. Attack": 80,
+    "Sp. Defense": 65,
+    "Speed": 80
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/charmeleon.png",
+    "front": "https://img.pokemondb.net/sprites/go/normal/charmeleon.png"
+  },
+},
+{
+  "id": 6,
+  "name": {
+    "english": "Charizard",
+    "japanese": "リザードン",
+    "chinese": "喷火龙",
+    "french": "Dracaufeu"
+  },
+  "type": [
+    "Fire",
+    "Flying"
+  ],
+  "base": {
+    "HP": 78,
+    "Attack": 84,
+    "Defense": 78,
+    "Sp. Attack": 109,
+    "Sp. Defense": 85,
+    "Speed": 100
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/platinum/back-normal/charizard.png",
+    "front": "https://img.pokemondb.net/sprites/go/normal/charizard-clone.png"
+  },
+},
+{
+  "id": 7,
+  "name": {
+    "english": "Squirtle",
+    "japanese": "ゼニガメ",
+    "chinese": "杰尼龟",
+    "french": "Carapuce"
+  },
+  "type": [
+    "Water"
+  ],
+  "base": {
+    "HP": 44,
+    "Attack": 48,
+    "Defense": 65,
+    "Sp. Attack": 50,
+    "Sp. Defense": 64,
+    "Speed": 43
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/platinum/back-shiny/bulbasaur.png",
+    "front": "https://img.pokemondb.net/sprites/home/shiny/bulbasaur.png"
+  },
+},
+{
+  "id": 8,
+  "name": {
+    "english": "Wartortle",
+    "japanese": "カメール",
+    "chinese": "卡咪龟",
+    "french": "Carabaffe"
+  },
+  "type": [
+    "Water"
+  ],
+  "base": {
+    "HP": 59,
+    "Attack": 63,
+    "Defense": 80,
+    "Sp. Attack": 65,
+    "Sp. Defense": 80,
+    "Speed": 58
+  },
+  "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/platinum/back-shiny/bulbasaur.png",
+    "front": "https://img.pokemondb.net/sprites/home/shiny/bulbasaur.png"
+  },
+},
+{
+  "id": 9,
+  "name": {
+    "english": "Blastoise",
+    "japanese": "カメックス",
+    "chinese": "水箭龟",
+    "french": "Tortank"
+  },
+  "type": [
+    "Water"
+  ],
+  "base": {
+    "HP": 79,
+    "Attack": 83,
+    "Defense": 100,
+    "Sp. Attack": 85,
+    "Sp. Defense": 105,
+    "Speed": 78
+  }, "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/platinum/back-shiny/bulbasaur.png",
+    "front": "https://img.pokemondb.net/sprites/home/shiny/bulbasaur.png"
+  },
+},
+{
+  "id": 10,
+  "name": {
+    "english": "Caterpie",
+    "japanese": "キャタピー",
+    "chinese": "绿毛虫",
+    "french": "Chenipan"
+  },
+  "type": [
+    "Bug"
+  ],
+  "base": {
+    "HP": 45,
+    "Attack": 30,
+    "Defense": 35,
+    "Sp. Attack": 20,
+    "Sp. Defense": 20,
+    "Speed": 45
+  }, "special": ['bind', 'bubble', 'agility', 'clamp'],
+  "frame": {
+    "back": "https://img.pokemondb.net/sprites/platinum/back-shiny/bulbasaur.png",
+    "front": "https://img.pokemondb.net/sprites/home/shiny/bulbasaur.png"
+  },
+}
 ]
 
 
