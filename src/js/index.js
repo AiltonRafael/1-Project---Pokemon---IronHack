cc1498537e699ecbd5bf866ee8e87e59df4d4aef
AiltonRafael <ailtonrafaeldiasjr@gmail.com>
2021-11-03 13:04:26 -0300

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import loadingScene from './scenes/loadingScene.js'
 import { gameScene } from './scenes/gameScene.js'
-import { BattleScene, BootScene, UIScene} from './scenes/battleScene.js'
 
 const config = {
     type: Phaser.AUTO,
@@ -19,9 +18,6 @@ const config = {
     scene: [
         loadingScene,
         gameScene,
-        BattleScene,
-        BootScene,
-        UIScene
         ]
     }
 
